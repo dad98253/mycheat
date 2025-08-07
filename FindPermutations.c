5b8a4b4e118d975528df7d53139cc479dceb8306
John Kuras <w7og@yahoo.com>
2025-08-06 21:40:55 -0700

    cosmetic changes only

--- FindPermutations.c
+++ FindPermutations.c
@@ -13,42 +13,45 @@
 
 #include "permute.h"
 
-
 void swap(char *a, char *b) {
-    char temp = *a;
-    *a = *b;
-    *b = temp;
+	char temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 void FindPermutations(char *str, int n, int r, int start, char *result) {
-    if (r == 0) {
-    	// add the word to our list
-    	*(Wordlist+NumWordsInList) = (char*)malloc(strlen(result+1));
-    	strcpy(*(Wordlist+NumWordsInList),result);
-    	NumWordsInList++;
-    	if (NumWordsInList>WordListSize){
+	if (r == 0) {
+		// add the word to our list
+		*(Wordlist + NumWordsInList) = (char*) malloc(strlen(result + 1));
+		strcpy(*(Wordlist + NumWordsInList), result);
+		NumWordsInList++;
+		if (NumWordsInList > WordListSize) {
 			perror("too many permutations found");
-			FCGI_printf("<h1>Program failed due to too many permutations found... this is a program bug!</h1>\n");
+			FCGI_printf(
+					"<h1>Program failed due to too many permutations found... this is a program bug!</h1>\n");
 			exit(EXIT_FAILURE);
-    	}
-        return;
-    }
+		}
+		return;
+	}
 
-    for (int i = start; i < n; i++) {
-    	// Swap characters to generate new permutations
-        swap((str + i), (str + start));
-        result[r - 1] = str[start];
-        FindPermutations(str, n, r - 1, start + 1, result);
-        // Backtrack - swap characters back to original position
-        swap((str + i), (str + start));
-    }
+	for (int i = start; i < n; i++) {
+		// Swap characters to generate new permutations
+		swap((str + i), (str + start));
+		result[r - 1] = str[start];
+		FindPermutations(str, n, r - 1, start + 1, result);
+		// Backtrack - swap characters back to original position
+		swap((str + i), (str + start));
+	}
 }
 
 // find the number of permutations P(n,m( = n!/(n-m)!)
-int Pnm(int n, int m){
-	if (m>n) return (-1);
-	if (n<1 || m<1) return (-2);
-	if (n>15) return (-3);
-	return (tgamma(n+1)/(tgamma(n-m+1)));
+int Pnm(int n, int m) {
+	if (m > n)
+		return (-1);
+	if (n < 1 || m < 1)
+		return (-2);
+	if (n > 15)
+		return (-3);
+	return (tgamma(n + 1) / (tgamma(n - m + 1)));
 }
 
--- mycheat.c
+++ mycheat.c
@@ -10,18 +10,17 @@
 #define BUFFER_SIZE 100000
 #define COMMAND_SIZE 1000
 
-int CompareString( const void *str1, const void *str2 );
+int CompareString(const void *str1, const void *str2);
 void SortWords(char* lines[], int count);
 int DedupeWords(char* lines[], long unsigned int* count);
-
 
 int main(void) {
 	char *data;
 	char letters[256]; // Buffer to store the extracted string of letters
-    char * str2;
-    char * str3;
-    char * result;
-    int n; // Take permutations of size n
+	char * str2;
+	char * str3;
+	char * result;
+	int n; // Take permutations of size n
 
 	while (FCGI_Accept() >= 0) {
 		FCGI_printf("Status: 200 OK\r\n");
@@ -61,7 +60,7 @@ int main(void) {
 					fread(data, 1, content_length, stdin);
 					data[content_length] = '\0'; // Null-terminate the string
 
-					// Parse the data (simple example for "letters=value")
+					// Parse the data ("letters=value")
 					char *name_start = strstr(data, "letters=");
 					if (name_start) {
 						name_start += strlen("letters=");
@@ -79,59 +78,64 @@ int main(void) {
 									"<h1>Bad input: the number of letters must be between 3 and 8</h1>\n");
 							exit(EXIT_FAILURE);
 						}
-
-
-
 						str2 = letters;
 						n = strlen(str2);
-						str3 = (char*)malloc(n+2);
-						result = (char*)calloc(n+2,1);
-					    if ( n < (int)maxWordSize ) maxWordSize = (long unsigned int)n;
+						str3 = (char*) malloc(n + 2);
+						result = (char*) calloc(n + 2, 1);
+						if (n < (int) maxWordSize)
+							maxWordSize = (long unsigned int) n;
 
-					    // check for min siz greater than max size
-					    if ( minWordSize > maxWordSize ) {
-					      	FCGI_printf("<h1> ///// ERROR : The minimum word size cannot be greater than the maximum word size.</h1>\n");
-					        exit(EXIT_FAILURE);
+						// check for min size greater than max size
+						if (minWordSize > maxWordSize) {
+							FCGI_printf(
+									"<h1> ///// ERROR : The minimum word size cannot be greater than the maximum word size.</h1>\n");
+							exit(EXIT_FAILURE);
 						}
 						// find all permutations taken r at a time where r goes from minWordSize to maxWordSize
-					    // first determin how many to expect
-					    for (int m = 3;m<=n;m++) {
-					        WordListSize+= Pnm( n,  m);
-					    }
-					    Wordlist = (char**)malloc( sizeof(char*) * (WordListSize+1) );
-					    for (int r=(int)minWordSize;r<(int)(maxWordSize+1);r++){
-					      strcpy(str3,str2);
-					      FindPermutations(str3, n, r, 0, result);
-					    }
-					    // now, put the list in alphabetical order
-					    SortWords(Wordlist, WordListSize);
-					    // and eliminate dupes
-					    DedupeWords(Wordlist, &WordListSize);
-					    // now check the spelling of each word in the list. If it appears in the dictionary, print it out.
-					    // Create a config
-					    AspellConfig *config = new_aspell_config();
-					    // Create a speller using the config
-					    AspellCanHaveError *possible_err = new_aspell_speller(config);
-					    if (aspell_error_number(possible_err) != 0) {
-					    	FCGI_printf("<h1>Aspell init error: %s</h1>\n", aspell_error_message(possible_err));
-					        delete_aspell_config(config);
-					        return EXIT_FAILURE;
-					    }
-					    // Cast to AspellSpeller after successful creation
-					    AspellSpeller *speller = to_aspell_speller(possible_err);
-					    for (long unsigned int m=0;m<WordListSize;m++){
-					        // Check if the word is correctly spelled
-					        int correct = aspell_speller_check(speller, Wordlist[m], strlen(Wordlist[m]));
-					        if (correct) {
-					        	FCGI_printf("<h1>%s</h1>\n", Wordlist[m]);
-					        }
-					        // we can free the words up while we go...
-					        free(Wordlist[m]);
-					    }
-					    // all done...
-					    free(str3);
-					    free(result);
-					    free(Wordlist);
+						// first determin how many to expect
+						for (int m = 3; m <= n; m++) {
+							WordListSize += Pnm(n, m);
+						}
+						Wordlist = (char**) malloc(
+								sizeof(char*) * (WordListSize + 1));
+						for (int r = (int) minWordSize;
+								r < (int) (maxWordSize + 1); r++) {
+							strcpy(str3, str2);
+							FindPermutations(str3, n, r, 0, result);
+						}
+						// now, put the list in alphabetical order
+						SortWords(Wordlist, WordListSize);
+						// and eliminate dupes
+						DedupeWords(Wordlist, &WordListSize);
+						// now check the spelling of each word in the list. If it appears in the dictionary, print it out.
+						// Create a config
+						AspellConfig *config = new_aspell_config();
+						// Create a speller using the config
+						AspellCanHaveError *possible_err = new_aspell_speller(
+								config);
+						if (aspell_error_number(possible_err) != 0) {
+							FCGI_printf("<h1>Aspell init error: %s</h1>\n",
+									aspell_error_message(possible_err));
+							delete_aspell_config(config);
+							return EXIT_FAILURE;
+						}
+						// Cast to AspellSpeller after successful creation
+						AspellSpeller *speller = to_aspell_speller(
+								possible_err);
+						for (long unsigned int m = 0; m < WordListSize; m++) {
+							// Check if the word is correctly spelled
+							int correct = aspell_speller_check(speller,
+									Wordlist[m], strlen(Wordlist[m]));
+							if (correct) {
+								FCGI_printf("<h1>%s</h1>\n", Wordlist[m]);
+							}
+							// we can free the words up while we go...
+							free(Wordlist[m]);
+						}
+						// all done...
+						free(str3);
+						free(result);
+						free(Wordlist);
 					} else {
 						FCGI_printf(
 								"<h1>Letters not found in POST data.</h1>\n");
@@ -152,30 +156,28 @@ int main(void) {
 	return EXIT_SUCCESS;
 }
 
-
-int CompareString( const void *str1, const void *str2 )
-{
-    char *const *pp1 = str1;
-    char *const *pp2 = str2;
-    return strcmp(*pp1, *pp2);
+int CompareString(const void *str1, const void *str2) {
+	char * const *pp1 = str1;
+	char * const *pp2 = str2;
+	return strcmp(*pp1, *pp2);
 }
 
-void SortWords(char* lines[], int count)
-{
-    qsort(lines, count, sizeof(*lines), CompareString);
+void SortWords(char* lines[], int count) {
+	qsort(lines, count, sizeof(*lines), CompareString);
 }
 
-int DedupeWords(char* lines[], long unsigned int* count){
-	if(*count < 2) return(*count);
-	for(long unsigned int i=0;i<(*count-1);i++){
-		if ( strcmp(lines[i],lines[i+1]) == 0 ) {
+int DedupeWords(char* lines[], long unsigned int* count) {
+	if (*count < 2)
+		return (*count);
+	for (long unsigned int i = 0; i < (*count - 1); i++) {
+		if (strcmp(lines[i], lines[i + 1]) == 0) {
 			// a dupe is found, delete it, move everyone up one, and decrement count
-			free(lines[i+1]);
-			if ( i < (*count+1) ) { // check for last word in list
-				for (long unsigned int j=i+1;j<(*count);j++) {
-					lines[j] = lines[j+1];
+			free(lines[i + 1]);
+			if (i < (*count + 1)) { // check for last word in list
+				for (long unsigned int j = i + 1; j < (*count); j++) {
+					lines[j] = lines[j + 1];
 				}
-			(*count)--;
+				(*count)--;
 			}
 		}
 	}
